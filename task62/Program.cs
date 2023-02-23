af255550619aaecbb0b9b204d72f4c1c0f596e21
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-23 20:44:57 +0300

    Task62:ready

--- task62/Program.cs
+++ task62/Program.cs
@@ -13,89 +13,49 @@ void PrintArray(int[,] array)
 
 void FillArray(int[,] array, int minValue, int maxValue)
 {
-    // for (int i = 0; i < array.GetLength(0); i++)
-    //     for (int j = 0; j < array.GetLength(1); j++)
-    //         array[i, j] = new Random().Next(minValue, maxValue + 1);
-
-    // while ((left < right) && flag)
-    // {
-    //     flag = false;
-    //     for (int j = left; j < right; j++)      // двигаемся слева направо
-    //     {
-    //         if (array[i, j] < array[i, j + 1])  // если следующий элемент больше текущего, меняем их местами
-    //         {
-    //             Swap(array, i, j, i, j + 1);
-    //             flag = true;                    // перемещения в этом цикле были
-    //         }
-    //     }
-    //     right--;                                // сдвигаем правую границу на предыдущий элемент
-    //     for (int j = right; j > left; j--)      //двигаемся справа налево
-    //     {
-    //         if (array[i, j - 1] < array[i, j])  // если предыдущий элемент меньше текущего, меняем их местами
-    //         {
-    //             Swap(array, i, j, i, j - 1);
-    //             flag = true;                    // перемещения в этом цикле были
-    //         }
-    //     }
-    //     left++;                                 // сдвигаем левую границу на следующий элемент
-    // }
-
     int count = 0;
 
     int m = array.GetLength(0);
     int n = array.GetLength(1);
 
-    int curRow = 0;
-    int curCol = 0;
     int left = 0;
-    int right = m - 1;
+    int right = n - 1;
     int up = 0;
-    int down = 0;
+    int down = m - 1;
 
     int dir = 0;  // 0 - from left to right, 
                   // 1 - from up to down, 
                   // 2 - from right to left,
                   // 3 - from down to up
-
-    while (count < 8 /*array.GetLength(0) * array.GetLength(1)*/)
+    while (((left <= right) && (up <= down)))
     {
-
-
-
-
         if (dir == 0)
         {
-            for (int j = curCol; j < n - curCol; j++)
-                array[curRow, j] = new Random().Next(minValue, maxValue + 1);
-            //curRow++;
-            //left++;
-            //right--;
+            for (int j = left; j <= right; j++)
+                array[up, j] = new Random().Next(minValue, maxValue + 1);
             up++;
             dir = 1;
         }
         else
             if (dir == 1)
         {
-            for (int i = curRow; i < m - curRow + 1; i++)
-                array[i, n - curCol - 1] = new Random().Next(minValue, maxValue + 1);
-            //curCol++;
+            for (int i = up; i <= down; i++)
+                array[i, right] = new Random().Next(minValue, maxValue + 1);
             right--;
             dir = 2;
         }
         else
              if (dir == 2)
         {
-            for (int j = n - curCol - 1; j > curCol - 1; j--)
-                array[m - curRow - 1, j] = new Random().Next(minValue, maxValue + 1);
-            //curRow++;
+            for (int j = right; j >= left; j--)
+                array[down, j] = new Random().Next(minValue, maxValue + 1);
             down--;
             dir = 3;
         }
         else
         {
-            for (int i = m - curRow; i > curRow; i--)
-                array[i, curCol] = new Random().Next(minValue, maxValue + 1);
-            //curCol++;
+            for (int i = down; i >= up; i--)
+                array[i, left] = new Random().Next(minValue, maxValue + 1);
             left++;
             dir = 0;
         }
@@ -112,4 +72,3 @@ int n = Convert.ToInt32(str[1]);
 
 int[,] arr = new int[m, n];
 FillArray(arr, 1, 9);
-//PrintArray(arr);
